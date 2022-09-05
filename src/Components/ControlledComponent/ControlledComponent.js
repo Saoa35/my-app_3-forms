02d4e25d54b2fb5a50bd5af22bbed41be6ae76b8
Saoa35 <saoamah@gmail.com>
2022-09-05 21:00:19 +0200

    + handleChange method

--- src/Components/ControlledComponent/ControlledComponent.js
+++ src/Components/ControlledComponent/ControlledComponent.js
@@ -3,11 +3,32 @@ import React from "react";
 
 class ControlledComponent extends React.Component {
 
+    state = {
+        value: ''
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+
+        this.setState({
+            value
+        })
+    }
+
     render() {
 
         return(
             <div>
                 <h3>Controlled Component</h3>
+
+                <form>
+                    <input type='text' value={this.state.value} onChange={this.handleChange} />
+                </form>
             </div>
         )
     }
