f411d5b4759331c7ddb3748ca26a7125de955a39
Saoa35 <saoamah@gmail.com>
2022-09-06 22:24:15 +0200

    created TestCSS component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,25 @@ class App extends React.Component {
   
   render() {
 
+    const color = 'red';
+
+    const inlineStyles = {
+      color
+
+    }
+
     return (
       <div className="App">
         {/* <ControlledComponent /> */}
         {/* <PropTypesEx name='Oleksandr' age={37} /> */}
+
+        <h2 style={inlineStyles}>
+          Text in h2 (App)
+        </h2>
+
+        <h3 className='h3-test'>Test text 1</h3>
+        <h3 className='h3-test'>Test text 2</h3>
+        <h3 className='h3-test'>Test text 3</h3>
       </div>
     );
   }
---
+++ src/Components/TestCSS/TestCSS.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class TestCSS extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+                <h3 className='h3-test'>Test text 2</h3>
+                <h3 className='h3-test'>Test text 3</h3>
+            </div>
+        )
+    }
+}
+
+export default TestCSS;
