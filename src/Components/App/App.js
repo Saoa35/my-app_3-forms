453b29d227bf5e0713eac01ebea0b944ce267019
Saoa35 <saoamah@gmail.com>
2022-09-06 21:47:01 +0200

    changed functional component App to class

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,20 @@
-// import ControlledComponent from '../ControlledComponent/ControlledComponent';
-import PropTypesEx from '../PropTypesEx/PropTypesEx';
+import React from 'react';
 import './App.css';
+// import ControlledComponent from '../ControlledComponent/ControlledComponent';
+// import PropTypesEx from '../PropTypesEx/PropTypesEx';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <ControlledComponent /> */}
-      <PropTypesEx name='Oleksandr' age={37} />
-    </div>
-  );
+
+class App extends React.Component {
+  
+  render() {
+
+    return (
+      <div className="App">
+        {/* <ControlledComponent /> */}
+        {/* <PropTypesEx name='Oleksandr' age={37} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
