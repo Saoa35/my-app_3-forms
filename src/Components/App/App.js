f47f493afeba11b6064dd0dcae79f64524cf3072
Saoa35 <saoamah@gmail.com>
2022-09-08 11:21:19 +0200

    commented some code

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import StyledComponents from '../StyledComponents/StyledComponents';
-import TestCSS from '../TestCSS/TestCSS';
+// import StyledComponents from '../StyledComponents/StyledComponents';
+// import TestCSS from '../TestCSS/TestCSS';
 import './App.css';
 // import ControlledComponent from '../ControlledComponent/ControlledComponent';
 // import PropTypesEx from '../PropTypesEx/PropTypesEx';
@@ -10,27 +10,27 @@ class App extends React.Component {
   
   render() {
 
-    const color = 'red';
+    // const color = 'red';
 
-    const inlineStyles = {
-      color
+    // const inlineStyles = {
+    //   color
 
-    }
+    // }
 
     return (
       <div className="App">
         {/* <ControlledComponent /> */}
         {/* <PropTypesEx name='Oleksandr' age={37} /> */}
 
-        <h2 style={inlineStyles}>
+        {/* <h2 style={inlineStyles}>
           Text in h2 (App)
-        </h2>
+        </h2> */}
 
-        <h3 className='h3-test'>Test text 1</h3>
+        {/* <h3 className='h3-test'>Test text 1</h3>
 
         <TestCSS />
 
-        <StyledComponents />
+        <StyledComponents /> */}
         
       </div>
     );
